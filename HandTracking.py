a7e87079b427e6af72bca0405f02f88489bf0f9b
Ali Sarikhany <112249748+hajalex@users.noreply.github.com>
2024-03-02 18:13:49 +0330

    Update HandTracking.py

--- HandTracking.py
+++ HandTracking.py
@@ -12,6 +12,11 @@ while True:
     results = hands.process(imgRGB)
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                # The location of each landmark
+                h, w, c = img.shape
+                cx, cy = int(lm.x*w), int(lm.y*h)
+                print(f'id:{id} --> {cx} {cy}')
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
 
     cv.imshow("image", img)
